aabc7da6ec21afbf6df9d76191406fda7605a5d2
Amanda Rosa Do Nascimento <amanda_nascimento150@estudante.sesisenai.org.br>
2025-11-11 15:14:24 -0300

    .

--- scripts.js
+++ scripts.js
@@ -110,7 +110,7 @@ function renderProducts() {
             <img src="${product.image}" alt="${product.name}" class="product-image">
             <div class="product-info">
                 <h3 class="product-title">${product.name}</h3>
-                <p class="product-description">R$ ${product.description || ''}</p>
+                <p class="product-description"> ${product.description || ''}</p>
                 <p class="product-price">R$ ${product.price.toFixed(2)}</p>
                 <button class="add-to-cart" data-id="${product.id}">Adicionar ao Carrinho</button>
             </div>
